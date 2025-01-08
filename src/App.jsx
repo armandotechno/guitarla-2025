431a0936d8061f65f6af33acdb49bf76e32f40f9
Armando Capriles <armando.capriles79@mppre.gob.ve>
2025-01-08 14:31:55 -0400

    Detectar si un elemento existe en el carrito

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ function App() {
     const [cart, setCart] = useState([])
 
     const addToCart = ( item ) => {
+
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        console.log(itemExists);
+        
+
         setCart(prevCart => [...prevCart, item])
     }
 
